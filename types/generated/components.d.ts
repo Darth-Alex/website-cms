f5896a3be6e1f72815f7f50b148ba7c50cfcde98
Alexander Rummel <rummel.alexander@it-factum.de>
2025-11-24 12:23:52 +0100

    Populate plugin

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,21 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface SharedNavLink extends Struct.ComponentSchema {
+  collectionName: 'components_shared_nav_links';
+  info: {
+    displayName: 'NavLink';
+  };
+  attributes: {
+    external: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    href: Schema.Attribute.Text & Schema.Attribute.Required;
+    Label: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'shared.nav-link': SharedNavLink;
+    }
+  }
+}
